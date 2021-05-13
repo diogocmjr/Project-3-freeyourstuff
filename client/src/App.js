8173a232dd50437de2061c1b42217f67ecf58966
Jan Colomer <colojan7@gmail.com>
2021-05-13 09:12:06 +0200

    Add log in, log out, sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Signup from './components/Signup';
+import Login from './components/Login'
+import Home from './components/Home'
+
+
+class App extends React.Component {
+
+  state = {
+    user: this.props.user
+  }
+
+  setUser = user => {
+    this.setState({ user })
+  }
+
+  render() {
+    return (
+      <>
+        <Route exact path='/'
+          render={props => <Home setUser={this.setUser} {...props} />}
+        />
+
+        <Route exact path='/signup'
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+
+        <Route exact path='/login' 
+          render={props => <Login setUser={this.setUser} {...props} />}
+        />
+      </>
+    )
+  }
 }
 
 export default App;
