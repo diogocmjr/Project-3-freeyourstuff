df3d6928e9fa4d833dde6b8b11fc7d679735d4e0
Jan Colomer <colojan7@gmail.com>
2021-05-17 20:14:19 +0200

    dashboard own items, edit profile check if user has location

--- client/src/components/OfferedItems.js
+++ client/src/components/OfferedItems.js
@@ -3,7 +3,7 @@ import { Link }from 'react-router-dom'
 
 export default function OfferedItems(props) {
 
-  const items = props.items.filter(item => item.owner === props.user._id).map(item => (
+  const items = props.items.filter(item => item.owner._id === props.user._id).map(item => (
     <div key={item._id}>
       <h1><Link to={`/items/${item._id}`}>{item.title}</Link></h1>
     </div>
